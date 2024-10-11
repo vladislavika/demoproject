b63cd3c1c4e7613dc75b4dbf0722702af8dbba9e
Volosey <94549754+Volosey@users.noreply.github.com>
2024-10-11 14:21:57 +0300

    Delete name

--- hello/forms.py
+++ hello/forms.py
@@ -1,5 +1,5 @@
 from django import forms
  
 class UserForm(forms.Form):
-    name = forms.CharField(help_text="Введите свое имя")
-    age = forms.IntegerField(help_text="Введите свой возраст")
+    name = forms.CharField()
+    age = forms.IntegerField()
